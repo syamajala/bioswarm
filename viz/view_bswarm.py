37899ee9ff983ef71a314f2a0639a010e5607123
Victor Frenkel <vgf2103@columbia.edu>
2012-10-21 21:39:07 -0400

    added keyframing of actor motion to script

--- viz/view_bswarm.py
+++ viz/view_bswarm.py
@@ -11,23 +11,32 @@ for line in sim_data:
     #break out of initializing loop when you hit next frame.
     if line.split(' ')[0] != "FRAME":
         tokens = line.split("; ")
-        pos_token = ast.literal_eval(tokens[3].split("=")[1])
+        pos = ast.literal_eval(tokens[3].split("=")[1])
         #scene = bpy.data.scenes.active
         
         #extract position from position token and use it to place cube (firefly)
         identifier = ""
-        if tokens[0] == "ACTOR":
-            identifier = "ACTOR_" + tokens[1].split("=")[1] + tokens[2].split("=")[1]
-        bpy.ops.mesh.primitive_cube_add(location=ast.literal_eval(tokens[3].split("=")[1]))
+        #if tokens[0] == "ACTOR":
+        identifier = "ACTOR_" + tokens[1].split("=")[1] + tokens[2].split("=")[1]
+        bpy.ops.mesh.primitive_cube_add(location=pos)
         ob = bpy.context.object
         ob.name = identifier
         #ob.show_name = True
     else:
         break;
 
-#for line in sim_data:
- #   if line.split(
+for line in sim_data:
+    if "FRAME" in line:
+        bpy.ops.anim.change_frame(frame=int(line.split(' ')[1]))
+    else:
+        tokens = line.split("; ")
+        identifier = "ACTOR_" + tokens[1].split("=")[1] + tokens[2].split("=")[1]
+        pos = ast.literal_eval(tokens[3].split("=")[1])
 
+        ob = bpy.data.objects[identifier]
+        #ob.select = True
+        ob.location = pos
+        bpy.ops.anim.keyframe_insert(type='Location')
 
 
 
